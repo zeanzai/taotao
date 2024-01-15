61d6cf6b38199ac42386397079b71705a9a9625e
zeanzai <zeanzai.me@gmail.com>
2024-01-15 16:03:43 +0800

    update

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/PictureService.java
+++
@@ -1,9 +0,0 @@
-package com.taotao.service;
-
-import java.util.Map;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface PictureService {
-    Map uploadFile(MultipartFile uploadFile);
-}
